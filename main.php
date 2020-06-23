f547a7aa38589e7cb59d6074ee2e69e48ae5ba17
Дмитрий Полищук <polishuk1003@gmail.com>
2020-06-23 22:52:01 +0300

    fix

--- main.php
+++ main.php
@@ -9,6 +9,12 @@ use Gtk3\Gtk\Container;
 use Gtk3\Gtk\Label;
 use Gtk3\Gtk\Box;
 
+function tmpCPU()
+{
+    $cpu = $cpu = (int)file_get_contents('/sys/class/thermal/thermal_zone0/temp');
+    return 'CPU=' . number_format($cpu/1000, 2);
+}
+
 $gtk = Gtk::getInstance();
 $gtk->init();
 
@@ -23,24 +29,32 @@ $signal->connect($window, 'destroy', [$gtk, 'mainQuit'], null);
 $container = new Container();
 
 $clockLabel = new Label(date('H:i:s'));
-$cpuLabel = new Label('');
+$cpuLabel = new Label(tmpCPU());
+$temperatureLabel = new Label('temperature');
+$pressureLabel = new Label('pressure');
+$humidityLabel = new Label('humidity');
 
 $hbox = new Box(Box::GTK_ORIENTATION_HORIZONTAL, 5);
 $vbox = new Box(Box::GTK_ORIENTATION_VERTICAL, 10);
 
-$vbox->packStart($cpuLabel, true, false, 5);
+
+$hbox->packStart($cpuLabel, true, false, 5);
+$hbox->packStart($temperatureLabel, true, false, 5);
+$hbox->packStart($pressureLabel, true, false, 5);
+$hbox->packStart($humidityLabel, true, false, 5);
+
+$vbox->packStart($hbox, true, false, 5);
 $vbox->packStart($clockLabel, true, false, 5);
 
-$hbox->packStart($vbox, true, false, 5);
-
-$container->add($window, $hbox);
+$container->add($window, $vbox);
 
 $window->showAll();
-
-$gtk->g_timeout_add(1000, function () use ($cpuLabel, $clockLabel) {
-    $cpu = (int)file_get_contents('/sys/class/thermal/thermal_zone0/temp');
-    $cpuLabel->setText('CPU=' . number_format($cpu/1000, 2));
+$i = 0;
+$gtk->g_timeout_add(1000, function () use ($cpuLabel, $clockLabel, &$i) {
+    $cpuLabel->setText(tmpCPU());
     $clockLabel->setText(date('H:i:s'));
+    $i++;
+    $clockLabel->setWidthChars($i);
     return true;
 }, null);
 
